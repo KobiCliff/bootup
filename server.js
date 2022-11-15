3c0bc7c6bb8f4ad48ee8fcdc123f4b7786eb2869
KobiCliff <kobi.cliff05@gmail.com>
2022-11-15 16:07:09 +0100

    updated mongoose

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.get("*", (req, res) => {
     res.send("<h1>We go live, this is second update since launch</h1>")
 })
 
-
+const DB = require("./module/dbconnect");
 
 const port = process.env.SERVER_PORT || 5000
 server.listen(port, () => {
